931e198e44b7eb45906c2b5f373ba715ec0bcd59
Jordan Fulghum <j.fulghum@gmail.com>
2020-04-10 14:35:24 -0400

    style changes

--- src/Notifications.svelte
+++ src/Notifications.svelte
@@ -1,6 +1,7 @@
 <script>
   import { notification } from "./store.js";
   import { onMount, onDestroy } from "svelte";
+  import { fade } from "svelte/transition";
 
   export let themes = {
     danger: "#E65D3B",
@@ -15,27 +16,6 @@
   let count = 0;
   let toasts = [];
   let unsubscribe;
-
-  function animateOut(node, { delay = 0, duration = 300 }) {
-    function vhTOpx(value) {
-      var w = window,
-        d = document,
-        e = d.documentElement,
-        g = d.getElementsByTagName("body")[0],
-        x = w.innerWidth || e.clientWidth || g.clientWidth,
-        y = w.innerHeight || e.clientHeight || g.clientHeight;
-
-      return (y * value) / 100;
-    }
-
-    return {
-      delay,
-      duration,
-      css: t =>
-        `opacity: ${(t - 0.5) *
-          1}; transform-origin: top right; transform: scaleX(${(t - 0.5) * 1});`
-    };
-  }
 
   function createToast(msg, theme, to) {
     const background = themes[theme] || themes["default"];
@@ -76,6 +56,7 @@
     bottom: 0;
     margin-bottom: 20px;
     z-index: 9999;
+    width: auto;
   }
 
   :global(.toasts) > .toast {
@@ -85,11 +66,11 @@
     box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1),
       0 10px 10px -5px rgba(0, 0, 0, 0.04);
     position: relative;
-    animation: animate-in 350ms forwards;
     color: #fff;
   }
 
   :global(.toasts) > .toast > .content {
+    color: #fff;
     padding: 12px;
     display: block;
   }
@@ -100,9 +81,6 @@
     background-color: transparent;
     height: 6px;
     width: 100%;
-    animation-name: shrink;
-    animation-timing-function: linear;
-    animation-fill-mode: forwards;
   }
 
   :global(.toasts) > .toast:before,
@@ -120,39 +98,17 @@
   :global(.toasts) > .toast:after {
     right: 10px;
     left: auto;
-    transform: skew(8deg) rotate(3deg);
-  }
-
-  @keyframes animate-in {
-    0% {
-      width: 0;
-      opacity: 0;
-      transform: scale(1.15) translateY(20px);
-    }
-    100% {
-      width: 40vw;
-      opacity: 1;
-      transform: scale(1) translateY(0);
-    }
-  }
-
-  @keyframes shrink {
-    0% {
-      width: 40vw;
-    }
-    100% {
-      width: 0;
-    }
   }
 </style>
 
 <ul class="toasts">
   {#each toasts as toast (toast.id)}
-    <li class="toast" style="background: {toast.background};" out:animateOut>
+    <li class="toast" style="background: {toast.background};" out:fade>
       <div class="content">{toast.msg}</div>
       <div
         class="progress"
         style="animation-duration: {toast.timeout}ms;"
+        transition:fade
         on:animationend={() => removeToast(toast.id)} />
     </li>
   {/each}
